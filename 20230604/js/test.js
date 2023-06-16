d32cf4a5da4576d68562b59f9e3e815b847b1286
emikwsm <32758528+emikwsm@users.noreply.github.com>
2023-06-16 10:25:50 +0900

    Add files via upload

---
+++ 20230604/js/test.js
@@ -0,0 +1,15 @@
+ 
+  // $(function () {
+  //   $("#send").click( function() {
+  //     // テキストボックスへ値を設定します
+  //     $("#text").val("");
+  //   });
+  // });
+
+  $(function () {
+    $("#send").click( function() {
+      // テキストボックスのデータを空にします
+      $("#text").val("");
+      $("#uname").val("");
+    });
+  });
